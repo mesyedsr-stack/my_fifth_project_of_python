c9e69bc755df0ec64136213741e38e80018211cf
Syed Sameer <mesyedsr@gmail.com>
2026-07-15 21:51:46 +0530

    Add files via upload

---
+++ sam09.py
@@ -0,0 +1,26 @@
+#names of cricketers characters
+number=int(input('enter your favourite cricketer jersey number'))
+if number==1:
+	print('KL rahul')
+elif number==2:
+	print('Arshdeep singh')
+elif number==3:
+	print('Yuzvendra chahal')
+elif number==4:
+	print('Abhishek sharma')
+elif number==5:
+	print('washington sundar')
+elif number==6:
+	print('Ruturag gaikward')
+elif number==7:
+	print('Ms dhoni')
+elif number==8:
+	print('Ravindra jadeja')
+elif number==9:
+	print('Sanju samsung')
+elif number==10:
+	print('Sachin tendulkar')
+else:
+	print('invalid number.print(below number 11)')
+
+
